689742d563e9be6e73b466a879bb0034d309e127
madison2829 <leemadison099@gmail.com>
2025-05-30 13:19:43 -0700

    Update CircleFinch.java

--- src/main/java/com/mnms/CircleFinch.java
+++ src/main/java/com/mnms/CircleFinch.java
@@ -4,7 +4,10 @@ public class CircleFinch {
     public static void main(String[] args){
         Finch bird = new Finch();
 
-        bird.setMove("F", 10, 50)
+
+        bird.setBeak(170,51,106);
+        bird.setMove("F", bird.getDistance() -4, 50);
+        bird.setTurn("L", 90, 50);
         bird.stop();
         bird.disconnect();
     }
